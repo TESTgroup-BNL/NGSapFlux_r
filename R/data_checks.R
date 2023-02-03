a12b4d4201ac67fe7a1b7b1a22b20b14c5adc52e
DavidsonKen <69693901+DavidsonKen@users.noreply.github.com>
2023-02-03 18:31:31 -0500

    Changes to load_data and sanity_check_course. Minor documentation updates

--- R/data_checks.R
+++ R/data_checks.R
@@ -1,23 +1,21 @@
-# Course QAQC processing. Identifies outliers and flags if data are messy by time period (e.g., 12:00:00-12:01:59) based on number of errant observations. Also detects if heater fired. Data returned as TRUE indicates an error 
-# This function is VERY SLOW, can be run in parallel to speed up
 ##' Course QAQC processing.
 ##' 
 ##' @name sanity_check_course
-##' @description Course QAQC processing. Identifies outliers and flags if data 
-##' are messy by time period (e.g., 12:00:00-12:01:59) based on number of errant 
-##' observations. Also detects if heater fired. Data returned as TRUE indicates 
-##' an error. NOTE: This function is VERY SLOW, can be run in parallel to 
-##' speed up 
+##' @description Course QAQC processing. Breaks each pulse interval into three periods, pre-pulse(00:00-02:00), heat (02:01-03:00), and post pre-pulse (02:01-15:00).
+##' Identifies and flags outliers as temperature readings that are greater than given amount above or below the mode temperature reading during that period. 
+##' If the number of outliers are exceed a user specified minimum threshold, the whole period is flagged as bad. 
+##' Data returned as TRUE indicates an error.
+##' 
 ##' 
 ##' @param df The input data frame
 ##' @param Dates The dates / date range to process
-##' @param Pre_Threshold_Temp PLACEHOLDER
-##' @param Pre_Threshold_Num PLACEHOLDER
-##' @param Post_Threshold_Temp PLACEHOLDER
-##' @param Post_Threshold_Num PLACEHOLDER
-##' @param HT_Temp PLACEHOLDER
+##' @param Pre_Threshold_Temp Difference in temp from mode (during the pre-pulse period) greater than which a point will be considered an outlier  
+##' @param Pre_Threshold_Num Minimum number of outliers during the pre-pulse period before data are flagged as bad
+##' @param Post_Threshold_Temp Difference in temp from mode (during the post-pulse period) greater than which a point will be considered an outlier  
+##' @param Post_Threshold_Num Minimum number of outliers during the post-pulse period before data are flagged as bad
+##' @param HT_Temp Temperature threshold for detecting heater pulse
 ##' 
-##' @return QAQC list
+##' @return A list composed of three QAQC data frames
 ##' @author Kenneth Davidson
 ##' @export
 ##' 
@@ -25,36 +23,35 @@ sanity_check_course <- function(df,Dates=unique(df$Date),Pre_Threshold_Temp=0.5,
                                 Pre_Threshold_Num=20, Post_Threshold_Temp=5, 
                                 Post_Threshold_Num=10, HT_Temp=0.5){
   
-  #!! TODO: should really develop file classes that we use instead of hard coding
   QC_Pre <- data.frame(Date= rep(as.character(Dates), each=48), 
-                       Pulse= rep(c('P01','P02','P03','P04','P05','P06','P07','P08','P09','P10','P11','P12','P13','P14','P15','P16','P17','P18','P19','P20','P21','P22','P23','P24',
-                                    'P25','P26','P27','P28','P29','P30','P31','P32','P33','P34','P35','P36','P37','P38','P39','P40','P41','P42','P43','P44','P45','P46','P47','P48'), times=length(Dates)),
-                       TREE1_TH1=NA,TREE2_TH1=NA,TREE3_TH1=NA,TREE4_TH1=NA,TREE5_TH1=NA,TREE1_TH2=NA,TREE2_TH2=NA,TREE3_TH2=NA,TREE4_TH2=NA,TREE5_TH2=NA,TREE1_TH3=NA,TREE2_TH3=NA,TREE3_TH3=NA,TREE4_TH3=NA,TREE5_TH3=NA)
+                       Pulse= rep(Pulses, times=length(Dates)),Tree_Blank)
   QC_Post <- data.frame(Date= rep(as.character(Dates), each=48), 
-                        Pulse= rep(c('P01','P02','P03','P04','P05','P06','P07','P08','P09','P10','P11','P12','P13','P14','P15','P16','P17','P18','P19','P20','P21','P22','P23','P24',
-                                     'P25','P26','P27','P28','P29','P30','P31','P32','P33','P34','P35','P36','P37','P38','P39','P40','P41','P42','P43','P44','P45','P46','P47','P48'), times=length(Dates)),
-                        TREE1_TH1=NA,TREE2_TH1=NA,TREE3_TH1=NA,TREE4_TH1=NA,TREE5_TH1=NA,TREE1_TH2=NA,TREE2_TH2=NA,TREE3_TH2=NA,TREE4_TH2=NA,TREE5_TH2=NA,TREE1_TH3=NA,TREE2_TH3=NA,TREE3_TH3=NA,TREE4_TH3=NA,TREE5_TH3=NA)
+                        Pulse= rep(Pulses, times=length(Dates)),Tree_Blank)
   QC_HT <- data.frame(Date= rep(as.character(Dates), each=48), 
-                      Pulse= rep(c('P01','P02','P03','P04','P05','P06','P07','P08','P09','P10','P11','P12','P13','P14','P15','P16','P17','P18','P19','P20','P21','P22','P23','P24',
-                                   'P25','P26','P27','P28','P29','P30','P31','P32','P33','P34','P35','P36','P37','P38','P39','P40','P41','P42','P43','P44','P45','P46','P47','P48'), times=length(Dates)),
-                      HT1=NA,HT2=NA,HT3=NA,HT4=NA,HT5=NA)
+                      Pulse= rep(Pulses, times=length(Dates)),Heat_Blank)
   
   for (j in as.character(Dates)){
-    for (k in c('P01','P02','P03','P04','P05','P06','P07','P08','P09','P10','P11','P12','P13','P14','P15','P16','P17','P18','P19','P20','P21','P22','P23','P24',
-                'P25','P26','P27','P28','P29','P30','P31','P32','P33','P34','P35','P36','P37','P38','P39','P40','P41','P42','P43','P44','P45','P46','P47','P48')){
-      MODE_Pre <- sapply(df[df$Date==j &df$Pulse==k&df$Baseline==T,3:17],FUN=getmode)
-      MODE_Post <- sapply(df[df$Date==j &df$Pulse==k&df$Baseline==F,3:17],FUN=getmode)
-      MODE_HT <- sapply(df[df$Date==j &df$Pulse==k&df$Heat==T,8:12],FUN=max)
+    for (k in Pulses){
+      MODE_Pre <- sapply(df[df$Date==j &df$Pulse==k&df$Baseline==T,colnames(Tree_Blank)],FUN=getmode)
+      MODE_Post <- sapply(df[df$Date==j &df$Pulse==k&df$Baseline==F,colnames(Tree_Blank)],FUN=getmode)
+      MODE_HT <- sapply(df[df$Date==j &df$Pulse==k&df$Heat==T,6:10],FUN=max)
       if(is.na(MODE_Pre)[1]){
-        QC_Pre[QC_Pre$Date==j &QC_Pre$Pulse==k,3:17] <- TRUE
-        QC_Post[QC_Post$Date==j &QC_Post$Pulse==k,3:17]  <- TRUE
-        QC_HT[QC_HT$Date==j &QC_HT$Pulse==k,3:7] <- TRUE
+        QC_Pre[QC_Pre$Date==j &QC_Pre$Pulse==k,colnames(Tree_Blank)] <- TRUE
+        QC_Post[QC_Post$Date==j &QC_Post$Pulse==k,colnames(Tree_Blank)]  <- TRUE
+        QC_HT[QC_HT$Date==j &QC_HT$Pulse==k,colnames(Heat_Blank)] <- TRUE
       }else{
-        QC_Pre_x <- df[df$Date==j &df$Pulse==k&df$Baseline==T,3:17]>= matrix(rep(t(data.frame(MODE_Pre+Pre_Threshold_Temp)),180), nrow=180,byrow = T)|df[df$Date==j &df$Pulse==k&df$Baseline==T,3:17]<=matrix(rep(t(data.frame(MODE_Pre-Pre_Threshold_Temp)),180), nrow=180,byrow = T)
-        QC_Pre[QC_Pre$Date==j &QC_Pre$Pulse==k,3:17]  <- colSums(QC_Pre_x, na.rm = TRUE)>=Pre_Threshold_Num
-        QC_Post_x<- df[df$Date==j &df$Pulse==k&df$Baseline==F,3:17]>=matrix(data= rep(MODE_Post+Post_Threshold_Temp,720),ncol=15, byrow=T)|df[df$Date==j &df$Pulse==k&df$Baseline==F,3:17]<=matrix(data= rep(MODE_Post-Post_Threshold_Temp,720),ncol=15, byrow=T)
-        QC_Post[QC_Post$Date==j &QC_Post$Pulse==k,3:17]  <- colSums(QC_Post_x, na.rm = TRUE)>=Post_Threshold_Num
-        QC_HT[QC_HT$Date==j &QC_HT$Pulse==k,3:7] <-  as.logical(MODE_HT-MODE_Pre[6:10]<=HT_Temp)
+        QC_Pre_x <- df[df$Date==j &df$Pulse==k&df$Baseline==T,colnames(Tree_Blank)]>= matrix(rep(t(data.frame(MODE_Pre+Pre_Threshold_Temp)),nrow(df[df$Date==j &df$Pulse==k&df$Baseline==T,])), nrow=nrow(df[df$Date==j &df$Pulse==k&df$Baseline==T,]),byrow = T)|
+          df[df$Date==j &df$Pulse==k&df$Baseline==T,colnames(Tree_Blank)]<=matrix(rep(t(data.frame(MODE_Pre-Pre_Threshold_Temp)),nrow(df[df$Date==j &df$Pulse==k&df$Baseline==T,])), nrow=nrow(df[df$Date==j &df$Pulse==k&df$Baseline==T,]),byrow = T)
+
+        QC_Pre[QC_Pre$Date==j &QC_Pre$Pulse==k,colnames(Tree_Blank)]  <- colSums(QC_Pre_x, na.rm = TRUE)>=Pre_Threshold_Num
+        
+        QC_Post_x<- df[df$Date==j &df$Pulse==k&df$Baseline==F,colnames(Tree_Blank)]>=matrix(data= rep(MODE_Post+Post_Threshold_Temp,nrow(df[df$Date==j &df$Pulse==k&df$Baseline==F,])),ncol=length(Tree_Blank), byrow=T)|df[df$Date==j &df$Pulse==k&df$Baseline==F,3:17]<=matrix(data= rep(MODE_Post-Post_Threshold_Temp,nrow(df[df$Date==j &df$Pulse==k&df$Baseline==F,])),ncol=length(Tree_Blank), byrow=T)
+        
+        nrow(df[df$Date==j &df$Pulse==k&df$Baseline==F,])
+        
+        QC_Post[QC_Post$Date==j &QC_Post$Pulse==k,colnames(Tree_Blank)]  <- colSums(QC_Post_x, na.rm = TRUE)>=Post_Threshold_Num
+        
+        QC_HT[QC_HT$Date==j &QC_HT$Pulse==k,colnames(Heat_Blank)] <-  as.logical(MODE_HT-MODE_Pre[6:10]<=HT_Temp)
       }
     }}
   QAQC <- list(QC_Pre,QC_Post,QC_HT)
@@ -62,20 +59,19 @@ sanity_check_course <- function(df,Dates=unique(df$Date),Pre_Threshold_Temp=0.5,
 }
 
 
-#Fine QAQC processing. Identifies outliers and flags if data are messy by individual point. Data returned as TRUE indicates an error 
-# This function is VERY SLOW, can be run in parallel to speed up
 ##' Fine QAQC processing
 ##' 
 ##' @name sanity_check_fine
-##' @description Fine QAQC processing. Identifies outliers and flags if data are 
-##' messy by individual point. Data returned as TRUE indicates an error.
-##' This function is VERY SLOW, can be run in parallel to speed up 
+##' @description Fine QAQC processing. Breaks each pulse interval into two periods, pre-pulse(00:00-02:00), and post pre-pulse (02:01-15:00).
+##' Identifies and flags outliers as temperature readings that are greater than given amount above or below the mode temperature reading during that period. 
+##' Output is a matrix, equivalent in dimensions to the input data, filled with binary values.
+##' Data returned as TRUE indicates an outlier.
 ##' 
 ##' 
 ##' @param df The input data frame
 ##' @param Dates The dates / date range to process 
-##' @param Pre_Threshold_Temp PLACEHOLDER
-##' @param Post_Threshold_Temp PLACEHOLDER
+##' @param Pre_Threshold_Temp Difference in temp from mode (during the pre-pulse period) greater than which a point will be considered an outlier
+##' @param Post_Threshold_Temp Difference in temp from mode (during the post-pulse period) greater than which a point will be considered an outlier
 ##' 
 ##' @return QAQC list
 ##' @author Kenneth Davidson
--- R/load_data.R
+++ R/load_data.R
@@ -1,54 +1,58 @@
 # Set up to run for each CR1000 box's data in a separate directory. Can be changed to process all boxes from one directory preferred
 
 
-#!!!! TODO - change from BoxA to something more generic
 #!!!! TODO - clean up function formatting
-##' The initial function to load raw CR1000 data
-##' Set up to run for each CR1000 box's data in a separate directory. 
-##' Can be changed to process all boxes from one directory preferred
+##' Function to load and format raw CR1000 data
 ##' 
 ##' @name load_data
-##' @description  function to load raw CR1000 data
+##' @description  function to load raw CR1000 data. 
+##' Set up to run for each CR1000 box's data in a separate directory. 
+##' Can be changed to process all boxes from one directory preferred
 ##' 
 ##' @param filepath The file path
 ##' @param Origin_DateTime e.g. 2000-01-01 01:00:00
 ##' @param End_DateTime e.g. 2030-01-01 01:00:00
 ##' 
-##' @return BoxA
+##' @return unprocData
 ##' 
 ##' @author Kenneth Davidson
+##' @author Jeremiah Anderson
 ##' @export
-load_Data <- function(filepath, Origin_DateTime="2000-01-01 01:00:00", End_DateTime="2030-01-01 01:00:00"){
+load_data <- function(filepath, Origin_DateTime="2000-01-01 01:00:00", End_DateTime="2030-01-01 01:00:00"){
   ls_files <- list.files(path=filepath, full.names = T) 
   Header <- read.csv(file=ls_files[1], header = F, skip=1,nrows = 1, as.is = T)
-  BoxA=do.call("rbind.fill", apply(X = as.matrix(ls_files),FUN = read.csv,MARGIN = 1, skip=4,header=F))
-  colnames(BoxA) <- Header
-  names(BoxA) <- c("TIMESTAMP", "RECORD", "TREE1_TH1", "TREE2_TH1", "TREE3_TH1", "TREE4_TH1", "TREE5_TH1",
-                   "TREE1_TH2", "TREE2_TH2", "TREE3_TH2", "TREE4_TH2", "TREE5_TH2", "TREE1_TH3", "TREE2_TH3", 
-                   "TREE3_TH3", "TREE4_TH3", "TREE5_TH3", "HT_START") # stock names for sapflux data frames
-  to_convert <- grep("TREE", names(BoxA))
-  BoxA$TIMESTAMP <- ymd_hms(BoxA$TIMESTAMP, tz="UTC")
-  BoxA <- BoxA[BoxA$TIMESTAMP >=as.POSIXct(Origin_DateTime,tz="UTC"),]
-  BoxA <- BoxA[BoxA$TIMESTAMP <as.POSIXct(End_DateTime,tz="UTC"),]
-  #BoxA <- BoxA[complete.cases(BoxA), ]
-  BoxA[to_convert] <- lapply(BoxA[to_convert], as.numeric)
-  BoxA[to_convert] <- lapply(BoxA[to_convert], convert_temp)
-  BoxA$Date <- date(BoxA$TIMESTAMP)
-  BoxA <- BoxA[!is.na(BoxA$Date),]
-  Full_Time_Range <- data.frame(TIMESTAMP=as.POSIXct(paste0(rep(unique(BoxA$Date), each=68400)," ", format(seq(as.POSIXct("00:00:01", format = "%T"), as.POSIXct("23:59:59", format = "%T"), by = "1 sec"), "%H:%M:%S")),tz="UTC"))
-  Full_Time_Range$min <- minute(Full_Time_Range$TIMESTAMP)
-  Full_Time_Range <- Full_Time_Range[Full_Time_Range$min %in% c(0:14,30:44),]
-  `%notin%` <- Negate(`%in%`)
-  New_Dates <- data.frame(TIMESTAMP=Full_Time_Range[which(Full_Time_Range$TIMESTAMP %notin% BoxA$TIMESTAMP),"TIMESTAMP"])
-  BoxA <- merge(BoxA,New_Dates, by="TIMESTAMP", all.x = T, all.y = T)
-  BoxA$Date <- date(BoxA$TIMESTAMP)
-  BoxA$Min <- minute(BoxA$TIMESTAMP)
-  BoxA$Time <- as_hms(BoxA$TIMESTAMP)
-  BoxA$Baseline <- BoxA$Min %in% c(0,1,2,30,31,32)
-  BoxA$Heat <- BoxA$Min %in% c(3,4,33,34)
-  BoxA$Pulse <- as.factor(paste0(hour(BoxA$Time),"_",minute(round_date(BoxA$TIMESTAMP, "30 minutes"))))
-  levels(BoxA$Pulse) <- c('P01','P02','P03','P04','P21','P22','P23','P24','P25','P26','P27','P28','P29','P30','P31','P32','P33','P34','P35','P36','P37','P38',
-                          'P39','P40','P05','P06','P41','P42','P43','P44','P45','P46','P47','P48','P07','P08','P09','P10','P11','P12','P13','P14','P15','P16','P17','P18','P19','P20')
-  return(BoxA)
+  unprocData=do.call(plyr::rbind.fill, apply(X = as.matrix(ls_files),FUN = read.csv,MARGIN = 1, skip=4,header=F))
+  colnames(unprocData) <- Header
+  names(unprocData) <- c("TIMESTAMP", "RECORD", "TREE1_TH1", "TREE2_TH1", "TREE3_TH1", "TREE4_TH1", "TREE5_TH1",
+                         "TREE1_TH2", "TREE2_TH2", "TREE3_TH2", "TREE4_TH2", "TREE5_TH2", "TREE1_TH3", "TREE2_TH3", 
+                         "TREE3_TH3", "TREE4_TH3", "TREE5_TH3", "HT_START") # stock names for sapflux data frames
+  to_convert <- grep("TREE", names(unprocData))
+  unprocData$TIMESTAMP <- lubridate::ymd_hms(unprocData$TIMESTAMP, tz="UTC")
+  unprocData <- unprocData[unprocData$TIMESTAMP >= as.POSIXct(Origin_DateTime,tz="UTC")|unprocData$TIMESTAMP <= as.POSIXct(End_DateTime,tz="UTC"),]
+  unprocData[to_convert] <- lapply(unprocData[to_convert], as.numeric)
+  unprocData[to_convert] <- lapply(unprocData[to_convert], convert_temp)
+  unprocData$Date <- lubridate::date(unprocData$TIMESTAMP)
+  unprocData <- unprocData[!is.na(unprocData$Date),]
+  #Full_Time_Range <- data.frame(TIMESTAMP=as.POSIXct(paste0(rep(unique(unprocData$Date), each=68400)," ", format(seq(as.POSIXct("00:00:01", format = "%T"), as.POSIXct("23:59:59", format = "%T"), by = "1 sec"), "%H:%M:%S")),tz="UTC"))
+  #Full_Time_Range$min <- lubridate::minute(Full_Time_Range$TIMESTAMP)
+  #Full_Time_Range <- Full_Time_Range[Full_Time_Range$min %in% c(0:14,30:44),]
+  #`%notin%` <- Negate(`%in%`)
+  #New_Dates <- data.frame(TIMESTAMP=Full_Time_Range[which(Full_Time_Range$TIMESTAMP %notin% unprocData$TIMESTAMP),"TIMESTAMP"])
+  #unprocData <- merge(unprocData,New_Dates, by="TIMESTAMP", all.x = T, all.y = T)
+  #unprocData$Date <- lubridate::date(unprocData$TIMESTAMP)
+  unprocData$Min <- lubridate::minute(unprocData$TIMESTAMP)
+  unprocData$Time <- hms::as_hms(unprocData$TIMESTAMP)
+  unprocData$Baseline <- unprocData$Min %in% c(0,1,30,31)
+  unprocData$Heat <- unprocData$Min %in% c(3,4,33,34)
+  unprocData$Pulse <- as.factor(paste0(lubridate::hour(unprocData$Time),"_",lubridate::minute(lubridate::round_date(unprocData$TIMESTAMP, "30 minutes"))))
+  levels(unprocData$Pulse) <- c('P01','P02','P03','P04','P21','P22','P23','P24','P25','P26','P27','P28','P29','P30','P31','P32','P33','P34','P35','P36','P37','P38',
+                                'P39','P40','P05','P06','P41','P42','P43','P44','P45','P46','P47','P48','P07','P08','P09','P10','P11','P12','P13','P14','P15','P16','P17','P18','P19','P20')
+  unprocData$Pulse <- as.character(unprocData$Pulse)
+  X <- aggregate(TIMESTAMP~Date,unprocData,length)
+  names(X)[2] <- "prop"
+  unprocData <- merge(unprocData,X, by="Date")
+  unprocData <- unprocData[unprocData$prop>=1000,]
+  
+  return(unprocData)
   
 }
