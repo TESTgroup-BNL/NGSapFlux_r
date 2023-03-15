e91771836eb65eab24ed8799633b93fec03d6fa8
Shawn P. Serbin <sserbin@bnl.gov>
2023-03-15 17:37:07 -0400

    Some cleanup of functions. bug squashing

--- R/load_data.R
+++ R/load_data.R
@@ -1,7 +1,3 @@
-# Set up to run for each CR1000 box's data in a separate directory. Can be changed to process all boxes from one directory preferred
-
-
-#!!!! TODO - clean up function formatting
 ##' Function to load and format raw CR1000 data
 ##' 
 ##' @name load_data
@@ -13,37 +9,64 @@
 ##' @param Origin_DateTime e.g. 2000-01-01 01:00:00
 ##' @param End_DateTime e.g. 2030-01-01 01:00:00
 ##' 
-##' @return A dataframe in standard format. These data are not yet processed or QAQCed
+##' @return A dataframe in standard NGSapFlux_r format. 
+##' These data are not yet processed or QAQC'ed
 ##' 
 ##' @author Kenneth Davidson
 ##' @author Jeremiah Anderson
+##' @author Shawn Serbin
+##' 
 ##' @export
-load_data <- function(filepath, Origin_DateTime="2000-01-01 01:00:00", End_DateTime="2030-01-01 01:00:00"){
-  ls_files <- list.files(path=filepath, full.names = T) 
-  Header <- read.csv(file=ls_files[1], header = F, skip=1,nrows = 1, as.is = T)
-  unprocData=do.call(plyr::rbind.fill, apply(X = as.matrix(ls_files),FUN = read.csv,MARGIN = 1, skip=4,header=F))
+load_data <- function(filepath, Origin_DateTime="2000-01-01 01:00:00", 
+                      End_DateTime="2030-01-01 01:00:00") {
+  ls_files <- list.files(path=file.path(filepath), full.names = T)
+  Header <- read.csv(file=ls_files[1], header = F, skip=1, nrows = 1, as.is = T)
+  # hard-coded skip below -could cause problems in the long run!!
+  unprocData <- do.call(plyr::rbind.fill, apply(X = as.matrix(ls_files),
+                                                FUN = read.csv, MARGIN = 1, 
+                                                skip=4, header=F))
   colnames(unprocData) <- Header
-  names(unprocData) <- c("TIMESTAMP", "RECORD", "TREE1_TH1", "TREE2_TH1", "TREE3_TH1", "TREE4_TH1", "TREE5_TH1",
-                         "TREE1_TH2", "TREE2_TH2", "TREE3_TH2", "TREE4_TH2", "TREE5_TH2", "TREE1_TH3", "TREE2_TH3", 
-                         "TREE3_TH3", "TREE4_TH3", "TREE5_TH3", "HT_START") # stock names for sapflux data frames
+  # stock names for sapflux data frames
+  # !!! FOR NOW DROPPING EXTRA COLUMN OF DATA !!!
+  #! DOING THIS TO MAKE SURE THE ASSIGNED NAMES MATCH NUM OF COLs
+  unprocData <- unprocData[,-dim(unprocData)[2]]
+  names(unprocData) <- c("TIMESTAMP", "RECORD", "TREE1_TH1", "TREE2_TH1", 
+                         "TREE3_TH1", "TREE4_TH1", "TREE5_TH1",
+                         "TREE1_TH2", "TREE2_TH2", "TREE3_TH2", "TREE4_TH2", 
+                         "TREE5_TH2", "TREE1_TH3", "TREE2_TH3", 
+                         "TREE3_TH3", "TREE4_TH3", "TREE5_TH3", "HT_START")
+  
   to_convert <- grep("TREE", names(unprocData))
   unprocData$TIMESTAMP <- lubridate::ymd_hms(unprocData$TIMESTAMP, tz="UTC")
-  unprocData <- unprocData[unprocData$TIMESTAMP >= as.POSIXct(Origin_DateTime,tz="UTC")|unprocData$TIMESTAMP <= as.POSIXct(End_DateTime,tz="UTC"),]
+  unprocData <- unprocData[unprocData$TIMESTAMP >= as.POSIXct(Origin_DateTime,
+                                                              tz="UTC") 
+                           & unprocData$TIMESTAMP <= as.POSIXct(End_DateTime,
+                                                                tz="UTC"),]
   unprocData[to_convert] <- lapply(unprocData[to_convert], as.numeric)
   unprocData[to_convert] <- lapply(unprocData[to_convert], convert_temp)
   unprocData$Date <- lubridate::date(unprocData$TIMESTAMP)
   unprocData <- unprocData[!is.na(unprocData$Date),]
   unprocData$Min <- lubridate::minute(unprocData$TIMESTAMP)
   unprocData$Time <- hms::as_hms(unprocData$TIMESTAMP)
+  # !!! these appear to be hard-coded assumptions that we should be confident about
   unprocData$Baseline <- unprocData$Min %in% c(0,1,30,31)
   unprocData$Heat <- unprocData$Min %in% c(3,4,33,34)
-  unprocData$Pulse <- as.factor(paste0(lubridate::hour(unprocData$Time),"_",lubridate::minute(lubridate::round_date(unprocData$TIMESTAMP, "30 minutes"))))
-  levels(unprocData$Pulse) <- c('P01','P02','P03','P04','P21','P22','P23','P24','P25','P26','P27','P28','P29','P30','P31','P32','P33','P34','P35','P36','P37','P38',
-                                'P39','P40','P05','P06','P41','P42','P43','P44','P45','P46','P47','P48','P07','P08','P09','P10','P11','P12','P13','P14','P15','P16','P17','P18','P19','P20')
+  # !!!
+  unprocData$Pulse <- as.factor(paste0(lubridate::hour(unprocData$Time),"_",
+                                       lubridate::minute(lubridate::round_date(
+                                         unprocData$TIMESTAMP, "30 minutes"))))
+  levels(unprocData$Pulse) <- c('P01','P02','P03','P04','P21','P22','P23','P24',
+                                'P25','P26','P27','P28','P29','P30','P31','P32',
+                                'P33','P34','P35','P36','P37','P38','P39','P40',
+                                'P05','P06','P41','P42','P43','P44','P45','P46',
+                                'P47','P48','P07','P08','P09','P10','P11','P12',
+                                'P13','P14','P15','P16','P17','P18','P19','P20')
   unprocData$Pulse <- as.character(unprocData$Pulse)
   X <- aggregate(TIMESTAMP~Date,unprocData,length)
-  names(X)[2] <- "prop"
+  names(X)[2] <- "prop" # !! THIS SHOULD HAVE A MORE DESCRIPTIVE NAME!!
   unprocData <- merge(unprocData,X, by="Date")
+  # !! it seems weird to just randomly do a data QA/QC subset here without 
+  # making it clear and days with low data are dropped! 
   unprocData <- unprocData[unprocData$prop>=1000,]
   
   return(unprocData)
--- R/utils.R
+++ R/utils.R
@@ -16,8 +16,8 @@ convert_temp <- function(x){
 ##' baseline temp for QAQC and data processing
 ##' 
 ##' @name getmode
-##' @description A helper function to find mode value to establish  pre and post heat 
-##' baseline temps for QAQC and data processing
+##' @description A helper function to find mode value to establish pre and 
+##' post heat baseline temps for QAQC and data processing
 ##' 
 ##' @author Kenneth Davidson
 ##' @export 
@@ -28,7 +28,8 @@ getmode <- function(v) {
 
 ##' A helper function for Heat_Dis_Spline
 ##' @name spline_fit
-##' @description A helper function to fit a spline to a moving window of data. Used estimate baseline temp of sap post heatting
+##' @description A helper function to fit a spline to a moving window of data. 
+##' Used estimate baseline temp of sap post heating
 ##' for detection of full pulse duration. 
 ##' 
 ##' @author Kenneth Davidson
@@ -37,4 +38,22 @@ spline_fit <- function(Y_vals,X_vals=Z$TIMESTAMP, X_Vals_New=X){
   Z1 <- smooth.spline(X_vals,Y_vals)
   Z2 <- predict(Z1, X_Vals_New)
   return(Z2$y)
+}
+
+# DO WE STILL NEED THIS FUNCTION??
+# Replaces values that were flagged in the fine QAQC with NA
+QAQC_Remove <- function(df,Pre_QAQC_df,Post_QAQC_df){
+  Z <- (sapply(Pre_QAQC_df[3:17],which))
+  X <- as.list(as.data.frame(sapply(Post_QAQC_df[3:17],which)))
+  df_1 <- df[df$Baseline==T,]
+  df_2 <- df[df$Baseline==F,]
+  for (i in colnames(df_1[,3:17])){
+    df_1[,i][Z[[i]]] <- NA
+  }
+  
+  for (i in colnames(df_2[,3:17])){
+    df_2[,i][X[[i]]] <- NA
+  }
+  df_3 <- rbind(df_1,df_2)
+  return(df_3)
 }
